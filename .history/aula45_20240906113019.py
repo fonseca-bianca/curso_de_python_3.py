1e1ee55c20d7143f70875c167fff807c1cc39900
Bianca Fonseca <b.bianca.fonseca@gmail.com>
2024-09-06 11:44:13 -0300

    Aula 45

---
+++ .history/aula45_20240906113019.py
@@ -0,0 +1,6 @@
+"""
+O 'for' por 'baixo dos panos'
+* iterador: sabe entregar um valor por vez
+* next: me entregue o próximo valor
+* iter: me entrege o seu iterador
+"""
---
+++ .history/aula45_20240906113118.py
@@ -0,0 +1,12 @@
+"""
+O 'for' por 'baixo dos panos'
+* iterador: sabe entregar um valor por vez
+* next: me entregue o próximo valor
+* iter: me entrege o seu iterador
+"""
+
+nome = "Luiz"
+numeros = range(0, 100, 8)
+
+for numero in numeros:
+    print(numero)
