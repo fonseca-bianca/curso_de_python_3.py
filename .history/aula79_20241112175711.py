48526afa0f5d3e67e26089788154ae9fdf78a8cc
Bianca Fonseca <b.bianca.fonseca@gmail.com>
2025-04-09 17:26:12 -0300

    Atualização aula 79

---
+++ .history/aula79_20241112175711.py
@@ -0,0 +1,7 @@
+""" 
+Exemplo de uso do tipo SET:
+O SET, em Python, facilita pra evitar a sobrecarga de espaço de armazenamento 
+de dados no programa, já que NÃO salva itens repetidos.
+Se, por ex., pedir pro usuário inserir algum dado e ele repetir, o SET irá armazenar
+somente o primeiro e manter ele na memória.
+"""
---
+++ .history/aula79_20241112183134.py
@@ -0,0 +1,14 @@
+""" 
+Exemplo de uso do tipo SET:
+O SET, em Python, facilita pra evitar a sobrecarga de espaço de armazenamento 
+de dados no programa, já que NÃO salva itens repetidos.
+Se, por ex., pedir pro usuário inserir algum dado e ele repetir, o SET irá armazenar
+somente o primeiro e manter ele na memória.
+"""
+
+letras = set()
+while True:
+    letra = input("Digite a letra: ")
+    letras.add(letra.lower())
+    
+    print(letras)
