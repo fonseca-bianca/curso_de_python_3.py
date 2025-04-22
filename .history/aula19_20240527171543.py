dd619c2a90e9d47a5da89d51e4d17c026ec5e501
Bianca Fonseca <b.bianca.fonseca@gmail.com>
2025-04-22 10:25:45 -0300

    Novas anotações aula 19

--- .history/aula19_20240527171543.py
+++ .history/aula19_20240527171543.py
@@ -4,10 +4,4 @@ menor = 1 < 2
 menor_ou_igual = 1 <= 2
 igual = 'a' == 'a'
 diferente = 'a' != 'b'
-
-print(maior)
 print(maior_ou_igual)
-print(menor)
-print(menor_ou_igual)
-print(igual)
-print(diferente)
