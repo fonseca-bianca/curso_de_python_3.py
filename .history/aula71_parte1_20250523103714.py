d42eb2563defe9845f6b7b922729029af9d43286
Bianca Fonseca <b.bianca.fonseca@gmail.com>
2025-05-23 10:49:57 -0300

    Alteração no cód e inclusão de novos comentários aula 71_parte1

---
+++ .history/aula71_parte1_20250523103714.py
@@ -0,0 +1,18 @@
+"""
+args - Argumentos NÃO nomeados
+* - *args (empacotamento e desempacotamento)
+
+"""
+# Desempacotamento:
+x, y, *resto = 1, 2, 3, 4
+print(x, y, resto)
+
+def soma(x, y):
+    return x + y
+
+print(soma(x, y)) # 3
+
+# Retorno:
+# 1 2 [3, 4] --> x = 1, y = 2, *resto = [3, 4]: o resto captura o restante 
+# e empacota como uma lista, pois não será usado no cód
+# 1
