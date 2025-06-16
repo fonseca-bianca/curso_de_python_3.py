075b0a2cda79f1c6b3a33b8b6fb53cbc2b27237e
Bianca Fonseca <b.bianca.fonseca@gmail.com>
2025-06-16 16:02:50 -0300

    Envio aula 83: Empac, Desempac de Dict + *args e **kwargs

---
+++ .history/aula83_20250613181926.py
@@ -0,0 +1,4 @@
+"""
+Empacotamento e desempacotamento de Dicionários + *args e **kwargs
+- **kwargs: keyword arguments (argumentos nomeados)
+"""
---
+++ .history/aula83_20250616104345.py
@@ -0,0 +1,5 @@
+"""
+Empacotamento e desempacotamento de Dicionários + *args e **kwargs
+- **kwargs: keyword arguments (argumentos nomeados)
+"""
+
